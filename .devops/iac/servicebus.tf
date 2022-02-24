b31e964fae68c349e5c722ca7762a1541a01377d
Darryl Matthews <Darryl.Matthews@mnp.ca>
2022-02-24 17:47:54 -0500

    20220224 Put sbq send key back in (for use by event relay)

--- .devops/iac/servicebus.tf
+++ .devops/iac/servicebus.tf
@@ -1,3 +1,4 @@
+# Create Service Bus namespace:
 resource "azurerm_servicebus_namespace" "sbnamespace" {
   name                = "fx-poc-servicebus-namespace"
   location            = azurerm_resource_group.rg.location
@@ -11,8 +12,19 @@ resource "azurerm_servicebus_namespace" "sbnamespace" {
   }
 }
 
+# Create client queue:
 resource "azurerm_servicebus_queue" "sbclientqueue" {
   name                = "fx_poc_client_queue"
   namespace_id        = azurerm_servicebus_namespace.sbnamespace.id
   enable_partitioning = true
+}
+
+# Create sending authorization rule for use by on-prem IntegrationEventRelay (may be replaced by RBAC later):
+resource "azurerm_servicebus_queue_authorization_rule" "sbclientqueueauthwriter" {
+  name     = "fx_poc_client_queue_authwriter"
+  queue_id = azurerm_servicebus_queue.sbclientqueue.id
+
+  listen = false
+  send   = true
+  manage = false
 }
